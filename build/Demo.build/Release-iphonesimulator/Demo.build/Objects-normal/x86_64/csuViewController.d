7e2de1c889666ad34e09ad313e2fc54613d42c47
k49918135 <k49918135@gmail.com>
2014-02-26 13:07:14 +0800

    tr
    trs

---
+++ build/Demo.build/Release-iphonesimulator/Demo.build/Objects-normal/x86_64/csuViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/k49918135/Desktop/iOS程式/Demo/Demo/csuViewController.m \
+  /Users/k49918135/Desktop/iOS程式/Demo/Demo/csuViewController.h
---
+++ build/Demo.build/Release-iphonesimulator/Demo.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: /Users/k49918135/Desktop/iOS程式/Demo/Demo/main.m \
+  /Users/k49918135/Desktop/iOS程式/Demo/Demo/csuAppDelegate.h
